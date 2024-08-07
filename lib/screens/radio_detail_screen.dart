51e6478ee36d5033150c38a5bc3da15b8cc09ccb
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2024-08-08 00:56:04 +0900

    ProgressBar

--- lib/screens/radio_detail_screen.dart
+++ lib/screens/radio_detail_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:just_audio/just_audio.dart';
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import '../viewmodels/radio_detail_viewmodel.dart';
 
 class RadioDetailScreen extends StatelessWidget {
@@ -28,7 +28,7 @@ class RadioDetailScreen extends StatelessWidget {
                 children: [
                   // 画像リストを表示
                   SizedBox(
-                    height: 200, // 必要に応じて高さを調整
+                    height: 200,
                     child: ListView.builder(
                       scrollDirection: Axis.horizontal,
                       itemCount: viewModel.radio!.imageUrls.length,
@@ -53,11 +53,57 @@ class RadioDetailScreen extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: Center(
-                      child: ElevatedButton(
-                        onPressed: () => _togglePlayPause(
-                            viewModel.radio!.audioUrl, context),
-                        child: Text('Play'),
+                      child: Consumer<RadioDetailViewModel>(
+                        builder: (context, state, child) {
+                          switch (state.audioState) {
+                            case AudioState.loading:
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: SizedBox(
+                                  height: 32,
+                                  width: 32,
+                                  child: CircularProgressIndicator(),
+                                ),
+                              );
+                            case AudioState.ready:
+                            case AudioState.paused:
+                              return IconButton(
+                                onPressed: () =>
+                                    context.read<RadioDetailViewModel>().play(),
+                                icon: Icon(Icons.play_arrow),
+                                iconSize: 32.0,
+                              );
+                            case AudioState.playing:
+                              return IconButton(
+                                onPressed: () => context
+                                    .read<RadioDetailViewModel>()
+                                    .pause(),
+                                icon: Icon(Icons.pause),
+                                iconSize: 32.0,
+                              );
+                            default:
+                              return SizedBox(
+                                height: 32,
+                                width: 32,
+                              );
+                          }
+                        },
                       ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Consumer<RadioDetailViewModel>(
+                      builder: (context, state, child) {
+                        return ProgressBar(
+                          progress: state.progressBarState.current,
+                          buffered: state.progressBarState.buffered,
+                          total: state.progressBarState.total,
+                          onSeek: (Duration position) {
+                            state.seek(position);
+                          },
+                        );
+                      },
                     ),
                   ),
                 ],
@@ -67,20 +113,5 @@ class RadioDetailScreen extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  void _togglePlayPause(String audioUrl, BuildContext context) {
-    final AudioPlayer _audioPlayer = AudioPlayer();
-    bool _isPlaying = false;
-
-    _audioPlayer.playerStateStream.listen((playerState) {
-      _isPlaying = playerState.playing;
-    });
-
-    if (_isPlaying) {
-      _audioPlayer.pause();
-    } else {
-      _audioPlayer.setUrl(audioUrl).then((_) => _audioPlayer.play());
-    }
   }
 }
--- lib/viewmodels/radio_detail_viewmodel.dart
+++ lib/viewmodels/radio_detail_viewmodel.dart
@@ -1,13 +1,129 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart';
 import '../models/radio.dart' as custom_radio;
 import '../services/radio_service.dart';
+
+enum AudioState {
+  ready,
+  paused,
+  playing,
+  loading,
+}
+
+class ProgressBarState {
+  ProgressBarState({
+    required this.current,
+    required this.buffered,
+    required this.total,
+  });
+
+  final Duration current;
+  final Duration buffered;
+  final Duration total;
+}
 
 class RadioDetailViewModel extends ChangeNotifier {
   final RadioService _radioService = RadioService();
   custom_radio.Radio? radio;
 
+  late AudioPlayer _audioPlayer;
+  ProgressBarState progressBarState = ProgressBarState(
+    current: Duration.zero,
+    buffered: Duration.zero,
+    total: Duration.zero,
+  );
+  AudioState audioState = AudioState.paused;
+  late StreamSubscription<PlayerState> _playerStateSubscription;
+  late StreamSubscription<ProgressBarState> _progressBarSubscription;
+
   Future<void> fetchRadioById(String id) async {
     radio = await _radioService.getRadioById(id);
+    if (radio != null) {
+      init();
+    }
     notifyListeners();
+  }
+
+  void init() {
+    _audioPlayer = AudioPlayer()..setUrl(radio!.audioUrl);
+    _listenToPlaybackState();
+    _listenForProgressBarState();
+  }
+
+  void play() => _audioPlayer.play();
+
+  void pause() => _audioPlayer.pause();
+
+  void seek(Duration position) => _audioPlayer.seek(position);
+
+  void setAudioState(AudioState state) {
+    audioState = state;
+    notifyListeners();
+  }
+
+  void setProgressBarState(ProgressBarState state) {
+    progressBarState = state;
+    notifyListeners();
+  }
+
+  void _listenToPlaybackState() {
+    _playerStateSubscription =
+        _audioPlayer.playerStateStream.listen((PlayerState state) {
+      if (isLoadingState(state)) {
+        setAudioState(AudioState.loading);
+      } else if (isAudioReady(state)) {
+        setAudioState(AudioState.ready);
+      } else if (isAudioPlaying(state)) {
+        setAudioState(AudioState.playing);
+      } else if (isAudioPaused(state)) {
+        setAudioState(AudioState.paused);
+      } else if (hasCompleted(state)) {
+        setAudioState(AudioState.paused);
+      }
+    });
+  }
+
+  void _listenForProgressBarState() {
+    _progressBarSubscription = CombineLatestStream.combine3(
+      _audioPlayer.positionStream,
+      _audioPlayer.bufferedPositionStream,
+      _audioPlayer.durationStream,
+      (Duration current, Duration buffer, Duration? total) => ProgressBarState(
+        current: current,
+        buffered: buffer,
+        total: total ?? Duration.zero,
+      ),
+    ).listen((ProgressBarState state) => setProgressBarState(state));
+  }
+
+  bool isLoadingState(PlayerState state) {
+    return state.processingState == ProcessingState.loading ||
+        state.processingState == ProcessingState.buffering;
+  }
+
+  bool isAudioReady(PlayerState state) {
+    return state.processingState == ProcessingState.ready && !state.playing;
+  }
+
+  bool isAudioPlaying(PlayerState state) {
+    return state.playing && !hasCompleted(state);
+  }
+
+  bool isAudioPaused(PlayerState state) {
+    return !state.playing && !isLoadingState(state);
+  }
+
+  bool hasCompleted(PlayerState state) {
+    return state.processingState == ProcessingState.completed;
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    _playerStateSubscription.cancel();
+    _progressBarSubscription.cancel();
+    super.dispose();
   }
 }
