a876c070b1d854bae614d7b458a34dc524711182
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2024-08-09 15:52:26 +0900

    Update radio_detail_viewmodel.dart

--- lib/viewmodels/radio_detail_viewmodel.dart
+++ lib/viewmodels/radio_detail_viewmodel.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
 import 'package:audio_service/audio_service.dart';
 import 'package:rxdart/rxdart.dart';
 import '../models/radio.dart' as custom_radio;
