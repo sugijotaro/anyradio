5b31c9e8f20b2d311c12ec94f020457aa1b7759f
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2024-08-11 15:39:28 +0900

    Update upload_screen.dart

--- lib/screens/upload_screen.dart
+++ lib/screens/upload_screen.dart
@@ -62,6 +62,7 @@ class _UploadScreenState extends State<UploadScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: Text(l10n.uploadImages),
+          toolbarHeight: 28.0,
         ),
         body: Consumer<UploadViewModel>(
           builder: (context, viewModel, child) {
